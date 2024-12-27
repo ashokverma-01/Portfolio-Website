6e3075692a8153f6c7b0b02d5cf826709bc82077
avfitnes <ashokvarma9636@gmail.com>
2024-12-27 16:26:14 +0530

    second commit deploy

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./Routes/userRoute");
 const cors = require("cors");
+const path = require("path");
+
+// deploye
+const _dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,6 +23,11 @@ app.use(cors());
 
 // Routes
 app.use("/api", userRoutes);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
